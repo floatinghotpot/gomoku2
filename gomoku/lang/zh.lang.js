256d2c3dc5c1021b9133ce5afce33793d7711206
Liming Xie <liming.xie@gmail.com>
2013-08-07 13:05:14 +0800

    optimize info dialog

--- gomoku/lang/zh.lang.js
+++ gomoku/lang/zh.lang.js
@@ -107,7 +107,7 @@ hotjs.i18n.put('zh', {
 	'gamerule':'游戏规则',
 	'gametip':'小窍门',
 	'about' : '关于',
-	'gamerule_text':'<ul><li>黑棋先走。</li><li>每下一局，双方交换先手。</li><li>5个同色棋子连成一线即获胜。</li><li>没有禁手。</li><li>可以提示，每次花费 1 个金币。</li><li>可以悔棋，每次花费 3 个金币。</li><ul>',
-	'gametip_text' : '<ul><li>手指可以缩放和移动网格。</li><li>点广告，有惊喜。</li></ul>',
+	'gamerule_text':'<ul><li>5个同色棋子连成一线即获胜。</li><li>黑棋先走。</li><li>每下一局，双方交换先手。</li><li>没有禁手。</li><li>可以提示，每次花费 1 个金币。</li><li>可以悔棋，每次花费 3 个金币。</li><ul>',
+	'gametip_text' : '<ul><li>点广告，有惊喜。</li><li>手指可以缩放和移动网格。</li><li>15x15是标准棋盘。</li><li>5位对手的棋力和风格不同。</li></ul>',
 	'about_text' : '<p>五子迷城, v2.0</p><p>安杰科技，智造乐趣</p><p><br/></p><p>官方微博: @rnjsoft</p><p>邮箱: rnjsoft.mobile@gmail.com</p>'
 });
